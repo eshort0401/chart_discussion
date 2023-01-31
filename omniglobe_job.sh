a16737539e0371346cb61833ac3393437d308e1a
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2023-01-31 13:48:24 +1100

    backup

--- omniglobe_job.sh
+++ omniglobe_job.sh
@@ -7,8 +7,8 @@
 #PBS -l walltime=04:00:00
 #PBS -l wd
 #PBS -l storage=gdata/w40+gdata/wr45+gdata/rt52+gdata/hh5+gdata/hj10+gdata/rq0
-#PBS -o ~/chart_discussion/mslp_logs/${MIN}_${MAX}_out
-#PBS -e ~/chart_discussion/mslp_logs/${MIN}_${MAX}_err
+#PBS -o "/home/563/esh563/chart_discussion/mslp_logs/${PBS_JOBID}_out"
+#PBS -e "/home/563/esh563/chart_discussion/mslp_logs/${PBS_JOBID}_out"
 
 python3 ~/chart_discussion/gen_omniglobe_fig_script.py -m $MIN -M $MAX
 for ((j=$MIN; j<$MAX; j++))

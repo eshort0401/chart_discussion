38acccd0b50e0757b8778b5f3e49fbccb8c0f0db
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2023-02-02 13:21:56 +1100

    back

--- omniglobe_job_bulk.sh
+++ omniglobe_job_bulk.sh
@@ -3,6 +3,6 @@ qsub -v MIN=1,MAX=24 omniglobe_job.sh
 qsub -v MIN=1,MAX=24 omniglobe_wind_job.sh
 for i in {24..216..24}
 do
-    qsub -v MIN=${i},MAX=$((i+24)) omniglobe_job.sh
-    qsub -v MIN=${i},MAX=$((i+24)) omniglobe_wind_job.sh
+    qsub -v MIN=${i},MAX=$((i+24)) ~/chart_discussion/omniglobe_job.sh
+    qsub -v MIN=${i},MAX=$((i+24)) ~/chart_discussion/omniglobe_wind_job.sh
 done
